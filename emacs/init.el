228404039b5a729126adc7b94d06a51b174c7860
Jeff Mataya <jeff.mataya@gmail.com>
2017-03-26 18:29:43 -0700

    Add language support for markdown

--- emacs/init.el
+++ emacs/init.el
@@ -27,7 +27,7 @@
     ("5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "36746ad57649893434c443567cb3831828df33232a7790d232df6f5908263692" "2a998a3b66a0a6068bcb8b53cd3b519d230dd1527b07232e54c8b9d84061d48d" "1dffeecd1565d04cd2059234e872cd80fcbe813488602d5c42b5c9e576924d9f" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" "9be1d34d961a40d94ef94d0d08a364c3d27201f3c98c9d38e36f10588469ea57" default)))
  '(package-selected-packages
    (quote
-    (git-gutter org-bullets yaml-mode solarized-theme magit helm-projectile projectile helm web-mode ensime use-package monokai-theme base16-theme evil-visual-mark-mode))))
+    (markdown-preview-mode markdown-mode git-gutter org-bullets yaml-mode solarized-theme magit helm-projectile projectile helm web-mode ensime use-package monokai-theme base16-theme evil-visual-mark-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
