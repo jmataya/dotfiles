d17645a9bcdbf474b63368505c9b8f1af9677de7
Jeff Mataya <jeff.mataya@gmail.com>
2018-01-05 23:55:11 -0500

    A bunch of configuration changes

--- emacs/init.el
+++ emacs/init.el
@@ -24,7 +24,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "25c242b3c808f38b0389879b9cba325fb1fa81a0a5e61ac7cae8da9a32e2811b" "ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" default)))
+    ("78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" "25c242b3c808f38b0389879b9cba325fb1fa81a0a5e61ac7cae8da9a32e2811b" "ef04dd1e33f7cbd5aa3187981b18652b8d5ac9e680997b45dc5d00443e6a46e3" default)))
  '(package-selected-packages
    (quote
     (ensime evil-leader yaml-mode web-mode use-package solarized-theme smart-mode-line-powerline-theme org-bullets neotree monokai-theme markdown-preview-mode magit load-theme-buffer-local key-chord helm-projectile go-mode exec-path-from-shell evil-visual-mark-mode evil-commentary color-theme-buffer-local base16-theme all-the-icons))))
