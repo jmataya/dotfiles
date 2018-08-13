4ba49a32876cddbca575f9eddb50b4fb07fccfec
Jeff Mataya <jmataya@redventures.com>
2018-08-12 23:30:19 -0400

    Bring back navigation settings

--- emacs/init.el
+++ emacs/init.el
@@ -35,7 +35,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
+    (helm-ebdb command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
