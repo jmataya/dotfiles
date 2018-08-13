8e6f09a3ba09d76b7d1312cac160f7aafca5a00d
Jeff Mataya <jmataya@redventures.com>
2018-08-12 22:27:14 -0400

    Set up environment, Go, and org configuration

--- emacs/init.el
+++ emacs/init.el
@@ -28,3 +28,17 @@
 ;;  ;; Your init file should contain only one such instance.
 ;;  ;; If there is more than one, they won't work right.
 ;;  )
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
