19181ebae94db2e2fb94522d21f5fa97acebbdd4
Jeff Mataya <jmataya@redventures.com>
2018-08-18 12:43:28 -0400

    Add a whole bunch of language settings

--- emacs/init.el
+++ emacs/init.el
@@ -35,10 +35,12 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (helm-ebdb command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
+    (vue-mode helm-ebdb command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:foreground: "#282a36" :background "#565761" :box nil))))
+ '(powerline-active1 ((t (:foreground: "#282a36" :background "#ff79c6" :box nil))))
+ '(powerline-active2 ((t (:foreground: "#282a36" :background "#ff79c6" :box nil)))))
