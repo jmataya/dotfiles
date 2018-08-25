69d12df7bb0758d565facbb3155163b0eeadcd24
Jeff Mataya <jmataya@redventures.com>
2018-08-25 14:19:24 -0400

    Better font configuration

--- emacs/init.el
+++ emacs/init.el
@@ -35,12 +35,13 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (vue-mode helm-ebdb command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
+    (web-mode vue-mode helm-ebdb command-log-mode go-test go-mode use-package toc-org scss-mode key-chord evil-org evil-leader dracula-theme dash))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((((min-colors 16777216)) (:background "#282a36" :foreground "#f8f8f2")) (t (:background "#000000" :foreground "#f8f8f2"))))
  '(mode-line ((t (:foreground: "#282a36" :background "#565761" :box nil))))
  '(powerline-active1 ((t (:foreground: "#282a36" :background "#ff79c6" :box nil))))
  '(powerline-active2 ((t (:foreground: "#282a36" :background "#ff79c6" :box nil)))))
