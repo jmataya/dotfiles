aa97023ce20cdccc10a41c039651507216722630
Jeff Mataya <jmataya@redventures.com>
2019-04-06 12:17:29 -0400

    Some experimentation around showing the terminal

---
+++ emacs/scratch.el
@@ -0,0 +1,18 @@
+(defun global-term ()
+  "Opens an ansi-term in bottom window that spreads the entire frame.
+   Create the window if it doesn't exist, otherwise show it."
+  (interactive)
+
+  (let* ((direction 'below)
+         (size 30)
+         (current-buf (get-buffer-window (current-buffer)))
+         (new-buf (generate-new-buffer-name "*global-term*"))
+         (term-win (split-window (frame-root-window)
+                                 (and size (prefix-numeric-value size))
+                                 direction)))
+    (select-window term-win)
+    (generate-new-buffer new-buf)
+    (set-window-dedicated-p current-buf nil)
+    (set-window-buffer current-buf new-buf)
+    (ansi-term "/bin/zsh" new-buf)))
+    
