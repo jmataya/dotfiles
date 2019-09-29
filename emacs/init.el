c7ee4f7a6f7175b2e74a78931e856a8188faef79
Jeff Mataya <jeff@jeffmataya.com>
2019-09-29 12:40:47 -0400

    Migrated colors into their own package

--- emacs/init.el
+++ emacs/init.el
@@ -13,6 +13,7 @@
 
 (require 'use-package)
 
+(add-to-list 'load-path "~/.emacs.d/gzy/colors/")
 (add-to-list 'load-path "~/.emacs.d/gzy/fonts/")
 
 (load-file "~/.emacs.d/modules/baseline.el")
@@ -22,6 +23,6 @@
 (load-file "~/.emacs.d/modules/magit.el")
 (load-file "~/.emacs.d/modules/navigation.el")
 (load-file "~/.emacs.d/modules/theme.el")
-(load-file "~/.emacs.d/modules/modeline.el")
+;; (load-file "~/.emacs.d/modules/modeline.el")
 
 (load-file "~/.emacs.d/modules/languages/lisp.el")
--- emacs/modules/theme.el
+++ emacs/modules/theme.el
@@ -1,39 +1,21 @@
-;; Set a nice base theme.
-;; I like light themes - sue me.
+;; Only do this when not in a terminal.
 
 (when window-system
+
+  ;; Set a nice base theme.
+  ;; I like light themes - sue me.
+
   (use-package base16-theme :ensure t)
   (setq base16-distinct-fringe-background nil)
-  (setq base16-highlight-mode-line 'contrast)
-  (load-theme 'base16-one-light t)
 
-  (defun gzy/theme-fringe ()
-    "Make sure the fringe is styled properly."
-    (set-face-attribute 'fringe nil
-                        :foreground (face-foreground 'default)
-                        :background (face-background 'default)))
+  (require 'gzy-colors)
+  (setq gzy-light-theme 'base16-one-light
+        gzy-dark-theme 'base16-onedark)
 
-  (gzy/theme-fringe)
+  (gzy-colors)
 
-  (defun gzy/light-theme ()
-    "Loads a light theme for the editor."
-    (interactive)
-    (load-theme 'base16-one-light t)
-    (gzy/theme-fringe))
+  ;; Set the font face.
 
-  (defun gzy/dark-theme ()
-    "Loads a dark theme for the editor."
-    (interactive)
-    (load-theme 'base16-onedark t)
-    (gzy/theme-fringe))
-
-  (global-set-key (kbd "C-c t l") 'gzy/light-theme)
-  (global-set-key (kbd "C-c t d") 'gzy/dark-theme))
-
-;; Set the font face.
-;; Only do this when we're not in a terminal.
-
-(when window-system
   (require 'gzy-fonts)
 
   (setq gzy-font-faces "Operator Mono SSm,Fira Code")
