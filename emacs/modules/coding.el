f3b4be0d239176a700ab0c2b5ad219d9bd48f560
Jeff Mataya <hi@jeffmataya.com>
2019-11-28 09:47:25 -0500

    Add company mode for auto-completion

--- emacs/modules/coding.el
+++ emacs/modules/coding.el
@@ -2,3 +2,8 @@
 
 (use-package evil-commentary :ensure t)
 (evil-commentary-mode)
+
+;; Company mode for autocompletion
+
+(use-package company :ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
---
+++ emacs/modules/company.el
@@ -0,0 +1,2 @@
+(use-package company :ensure t)
+(add-hook 'after-init-hook 'global-company-mode)
