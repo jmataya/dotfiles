157a092ea80c0f43f8f26e7e703d034bd4574145
Jeff Mataya <hi@jeffmataya.com>
2020-03-27 07:52:47 -0400

    Added markdown settings

--- emacs/init.el
+++ emacs/init.el
@@ -32,14 +32,14 @@
 (load-file "~/.emacs.d/modules/writing.el")
 ;; (load-file "~/.emacs.d/modules/modeline.el")
 
+(load-file "~/.emacs.d/modules/languages/clojure.el")
 (load-file "~/.emacs.d/modules/languages/go.el")
 (load-file "~/.emacs.d/modules/languages/javascript.el")
 (load-file "~/.emacs.d/modules/languages/lisp.el")
+(load-file "~/.emacs.d/modules/languages/markdown.el")
 (load-file "~/.emacs.d/modules/languages/org-mode.el")
 (load-file "~/.emacs.d/modules/languages/typescript.el")
 
-(require 'gzy-modeline)
-(gzy-modeline)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -50,7 +50,7 @@
     ("c614d2423075491e6b7f38a4b7ea1c68f31764b9b815e35c9741e9490119efc0" "6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "16dd114a84d0aeccc5ad6fd64752a11ea2e841e3853234f19dc02a7b91f5d661" default)))
  '(package-selected-packages
    (quote
-    (ox-gfm writeroom-mode markdown-mode+ ztree treemacs prettier-js vala-mode yaml-mode web-mode vue-mode use-package toc-org spaceline solarized-theme scss-mode rjsx-mode rainbow-delimiters paredit org-bullets neotree markdown-mode magit key-chord helm-projectile helm-ag gotest ggtags exec-path-from-shell evil-leader evil-commentary dracula-theme dockerfile-mode diminish counsel-projectile clojure-mode-extra-font-locking cider base16-theme all-the-icons))))
+    (clojure-mode treeview ox-gfm writeroom-mode markdown-mode+ ztree treemacs prettier-js vala-mode yaml-mode web-mode vue-mode use-package toc-org spaceline solarized-theme scss-mode rjsx-mode rainbow-delimiters paredit org-bullets neotree markdown-mode magit key-chord helm-projectile helm-ag gotest ggtags exec-path-from-shell evil-leader evil-commentary dracula-theme dockerfile-mode diminish counsel-projectile clojure-mode-extra-font-locking cider base16-theme all-the-icons))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
