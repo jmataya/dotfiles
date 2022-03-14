7e47c61edd26809c34b0a8464ca8f391ad16ecc1
Jeff Mataya <hi@jeffmataya.com>
2022-03-14 08:50:49 -0400

    Local changes

--- emacs/modules/theme.el
+++ emacs/modules/theme.el
@@ -28,7 +28,7 @@
 
     (require 'gzy-fonts)
 
-    (setq gzy-font-faces "Fira Code,Operator Mono SSm Light")
+    (setq gzy-font-faces "CaskaydiaCove Nerd Font Mono,Fira Code,Operator Mono SSm Light")
     (setq gzy-line-spacing 0.4)
 
     (if (memq window-system '(mac ns))
